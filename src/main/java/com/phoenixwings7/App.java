74f803946def9f44db47e507802020d277f46bf8
PhoenixWings7 <adamus.klaudia.pasp@gmail.com>
2021-05-20 13:29:31 +0200

    create first project structure

---
+++ src/main/java/com/phoenixwings7/App.java
@@ -0,0 +1,8 @@
+package com.phoenixwings7;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    }
+}
---
+++ src/test/java/com/phoenixwings7/AppTest.java
@@ -0,0 +1,10 @@
+package com.phoenixwings7;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/* These tests follow this naming convention: should_expectedBehavior_when_stateUnderTest */
+
+public class AppTest
+{}
