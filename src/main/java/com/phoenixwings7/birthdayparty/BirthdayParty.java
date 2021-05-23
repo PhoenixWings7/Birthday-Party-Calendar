77469d61e889974a07f9066243b8e2e839fea762
PhoenixWings7 <adamus.klaudia.pasp@gmail.com>
2021-05-23 15:49:16 +0200

    create Person & BirthdayParty classes

---
+++ src/main/java/com/phoenixwings7/birthdayparty/BirthdayParty.java
@@ -0,0 +1,44 @@
+package com.phoenixwings7.birthdayparty;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class BirthdayParty {
+    private final LocalDate birthdayDate;
+
+    BirthdayParty(LocalDate birthdayDate) {
+        this.birthdayDate = birthdayDate;
+    }
+
+    BirthdayParty(int day, int month, int year) {
+        this.birthdayDate = LocalDate.of(year, month, day);
+    }
+
+    public LocalDate getBirthdayPartyDate() {
+        final int currentYear = LocalDate.now().getYear();
+        return getBirthdayPartyDate(currentYear);
+    }
+
+    public LocalDate getBirthdayPartyDate(int year) {
+        LocalDate birthdayThisYear = LocalDate.of(year,
+                birthdayDate.getMonth(), birthdayDate.getDayOfMonth());
+
+        LocalDate partyDate;
+        DayOfWeek dayOfWeek = birthdayThisYear.getDayOfWeek();
+        if ((dayOfWeek == DayOfWeek.SUNDAY) || (dayOfWeek == DayOfWeek.SATURDAY)) {
+            // if birthday is on Saturday or Sunday, return the date
+            partyDate = birthdayThisYear;
+        }
+        else {
+            int daysToSaturday = daysToSaturday(dayOfWeek);
+            partyDate = birthdayThisYear.plusDays(daysToSaturday);
+        }
+
+        return partyDate;
+    }
+
+    private int daysToSaturday(DayOfWeek dayOfWeek) {
+        return DayOfWeek.SATURDAY.getValue() - dayOfWeek.getValue();
+    }
+
+}
---
+++ src/main/java/com/phoenixwings7/birthdayparty/Person.java
@@ -0,0 +1,58 @@
+package com.phoenixwings7.birthdayparty;
+
+import com.phoenixwings7.customexceptions.NullDateException;
+
+import java.time.LocalDate;
+
+public class Person {
+    String name;
+    LocalDate birthday;
+    BirthdayParty birthdayParty;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public LocalDate getBirthdayPartyDateThisYear() throws NullDateException {
+        if (birthday == null) {
+            throw new NullDateException();
+        }
+        if(birthdayParty == null) {
+            birthdayParty = new BirthdayParty(birthday);
+        }
+
+        return birthdayParty.getBirthdayPartyDate();
+    }
+
+    public String getBirthdayPartyDate(int year) throws NullDateException {
+        if (birthday == null) {
+            throw new NullDateException();
+        }
+        if(birthdayParty == null) {
+            birthdayParty = new BirthdayParty(birthday);
+        }
+
+        return birthdayParty.getBirthdayPartyDate(year).toString();
+    }
+}
