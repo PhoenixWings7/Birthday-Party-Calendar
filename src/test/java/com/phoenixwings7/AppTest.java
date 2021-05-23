329aab0062588e0ffe645dae0a4603943e8eb748
PhoenixWings7 <adamus.klaudia.pasp@gmail.com>
2021-05-23 15:53:29 +0200

    add tests

--- src/test/java/com/phoenixwings7/AppTest.java
+++ src/test/java/com/phoenixwings7/AppTest.java
@@ -2,9 +2,33 @@ package com.phoenixwings7;
 
 import static org.junit.Assert.assertTrue;
 
+import com.phoenixwings7.birthdayparty.Person;
+import com.phoenixwings7.customexceptions.NullDateException;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.time.LocalDate;
 
 /* These tests follow this naming convention: should_expectedBehavior_when_stateUnderTest */
 
 public class AppTest
-{}
+{
+    private static Person person;
+    @Before
+    public void setUp() throws Exception {
+        person = new Person("Alice");
+    }
+
+    @Test
+    public void should_calculateCorrectly_when_providedCorrectData() throws NullDateException {
+        LocalDate dayToExamine = LocalDate.of(2021, 5, 20);
+        LocalDate saturdayThatWeek = dayToExamine.plusDays(2);
+        person.setBirthday(dayToExamine);
+        assertTrue(saturdayThatWeek.isEqual(person.getBirthdayPartyDateThisYear()));
+    }
+
+    @Test(expected = NullDateException.class)
+    public void should_throwException_when_birthdayIsNull() throws NullDateException {
+        person.getBirthdayPartyDateThisYear();
+    }
+}
